acda91ab9bb542fe4ac6b8044f65667f7ca27cec
Solomon Bekele <solubman28@gmail.com>
2025-11-11 15:48:06 +0400

    practicing

--- vanila.js
+++ vanila.js
@@ -2,3 +2,27 @@ const heading = document.createElement('h1');
         heading.innerHTML='this is react';
         const root = document.getElementById('root');
         root.appendChild(heading)
+
+// using react create element
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement(
+    "div",
+    { id: "child", key: "child1" }, // 👈 added key here
+    [
+      React.createElement("h1", { key: "h1-1" }, "I am H1 tag"),
+      React.createElement("h2", { key: "h2-1" }, "I am H2 tag"),
+    ]
+  ),
+  React.createElement(
+    "div",
+    { id: "child2", key: "child2" }, // 👈 another key here
+    [
+      React.createElement("h1", { key: "h1-2" }, "I am H1 tag"),
+      React.createElement("h2", { key: "h2-2" }, "I am H2 tag"),
+    ]
+  ),
+]);
+
+// const heading = React.createElement("h1", {id:"heading",className:"heading"}, "this is react cdn");
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
